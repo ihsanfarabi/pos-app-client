9cc7755a9d31bbbe9119657b2cca394853a4e2a2
ihsanfarabi <34181167+ihsanfarabi@users.noreply.github.com>
2025-10-12 00:26:05 +0700

    Update packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 const projectRoot = fileURLToPath(new URL('.', import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(projectRoot, 'src'),
